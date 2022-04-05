e289409619b94245385ccd6689eb2e9ed141f2e6
KateSilva26 <102698823+KateSilva26@users.noreply.github.com>
2022-04-05 00:11:27 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+
+from BOLETA import Boleta
+print("                                                                        ")
+print("****************************DATOS DE ENTRADA****************************")
+
+Boleta_Pago = Boleta()
+
+print("                                                                        ")
+print("************************************************************************")
+print("*****************************BOLETA DE PAGO*****************************")
+Boleta_Pago.nom_trab()
+Boleta_Pago.categoria()
+Boleta_Pago.Pph()
+Boleta_Pago.DescuentoT()
+Boleta_Pago.SueldoNeto()
